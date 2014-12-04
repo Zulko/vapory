8604afeeec8e6a4c75635711fa9e29a3735505da
Reuben Thomas <reuben@crunchbang>
2014-12-04 18:20:29 +0000

    updated __init__ with new name of WIKIREF

--- vapory/__init__.py
+++ vapory/__init__.py
@@ -8,6 +8,6 @@ from .version import __version__
 __all__ = [k for k in locals().keys() if k not in
            ['webbrowser',
             'deepcopy',
-            'wikiref',
+            'WIKIREF',
             'vectorize',
             'format_if_necessary']]
