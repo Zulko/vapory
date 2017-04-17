29e208efa2d4d37ae38a127871a540c44455e6a1
Eric Lunderberg <lunderberg@nscl.msu.edu>
2017-04-17 13:20:43 -0400

    Added option to enable alpha channel of POV-Ray output

--- vapory/io.py
+++ vapory/io.py
@@ -54,7 +54,8 @@ def ppm_to_numpy(filename=None, buffer=None, byteorder='>'):
 
 def render_povstring(string, outfile=None, height=None, width=None,
                      quality=None, antialiasing=None, remove_temp=True,
-                     show_window=False, tempfile=None, includedirs=None):
+                     show_window=False, tempfile=None, includedirs=None,
+                     output_alpha=False):
 
     """ Renders the provided scene description with POV-Ray.
 
@@ -99,6 +100,7 @@ def render_povstring(string, outfile=None, height=None, width=None,
     if width is not None: cmd.append('+W%d'%width)
     if quality is not None: cmd.append('+Q%d'%quality)
     if antialiasing is not None: cmd.append('+A%f'%antialiasing)
+    if output_alpha: cmd.append('Output_Alpha=on')
     if not show_window:
         cmd.append('-D')
     else:
--- vapory/vapory.py
+++ vapory/vapory.py
@@ -58,7 +58,7 @@ class Scene:
     def render(self, outfile=None, height=None, width=None,
                      quality=None, antialiasing=None, remove_temp=True,
                      auto_camera_angle=True, show_window=False, tempfile=None,
-                     includedirs=None):
+                     includedirs=None, output_alpha=False):
 
         """ Renders the scene to a PNG, a numpy array, or the IPython Notebook.
 
@@ -84,7 +84,7 @@ class Scene:
 
         return render_povstring(str(self), outfile, height, width,
                                 quality, antialiasing, remove_temp, show_window,
-                                tempfile, includedirs)
+                                tempfile, includedirs, output_alpha)
 
 
 class POVRayElement:
